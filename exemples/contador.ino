4d12568a7dfd537bc71ea6d256ce9d4d45c68280
barrak2018 <joseestebanojedaaular@gmail.com>
2022-04-14 00:45:32 -0400

    arreglo del ejemplo contador

--- exemples/contador.ino
+++ exemples/contador.ino
@@ -1,15 +1,14 @@
 #include "Easey7display.h"
 Display d1(2,3,4,5,6,7,8,true);
-int pot = A5;
-void setup() {
-  //Serial.begin(9600);
-  // put your setup code here, to run once:
-  //Serial.begin(9600);
 
+void setup() {
 }
 void loop() {
-  int num = map(analogRead(pot),0,1023,0,10);
-  delay(num);
-  d1.print(num);
+  for (int i = 0; i < 10; i++)
+  {
+    d1.print(i);
+    delay(1000);
+  }
+  
   
   }//:)
